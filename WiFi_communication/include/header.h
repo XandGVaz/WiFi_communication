e389effb64b28730ccf641d63dff110f96c8619a
XandGVaz <vitorgarciavaz@gmail.com>
2025-01-14 11:37:23 -0300

    modify variable names

--- WiFi_communication/include/header.h
+++ WiFi_communication/include/header.h
@@ -1,20 +1,22 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+/*===============================================================================*/
 // Bibliotecas
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include "html.h"
 
-// Pinos dos leds
+/*===============================================================================*/
+// Configuração de pinos
 #define PIN_LED 2
+#define DHT_PIN 13
 
-// IP, gateway e DNS da ESP 32
-IPAddress LocalIP (192,168,1,100);
-IPAddress Gateway (192,168,1,10);
-IPAddress Subnet (255,255,255,0);
-
+/*===============================================================================*/
 // Informações da rede WiFi transmitida pelo roteador
 const String ssidRounter =  "Usa 3G folgado_2.4GHZ"; // nome da rede
 const String passwordRounter =  "soentrabonito";     // senha
@@ -25,7 +27,16 @@ String Sended;
 // Servidor criado
 WebServer Server(80);
 
-// Funções
+/*===============================================================================*/
+// Sensor DHT11
+DHT_Unified DHT(DHT_PIN, DHT11);
+
+//Temperatura e umidade
+String Temperature; 
+String Humidity;
+
+/*===============================================================================*/
+// Funções do servidor
 void WiFiConnect();
 void handleBlink();
 void handleSubmit();
@@ -34,5 +45,16 @@ void handleUpdate();
 void createServer();
 void handleClient();
 void sendData();
+
+/*===============================================================================*/
+// Funções do sensor
+sensor_t Sensor;
+void setupDHT();
+void updateValueDHT();
+
+/*===============================================================================*/
+// Delay de funcionamento do sistema
+#define delayMS 100
+uint32_t delayDHT;
 
 #endif
--- WiFi_communication/src/main.cpp
+++ WiFi_communication/src/main.cpp
@@ -1,12 +1,12 @@
 #include "header.h"
 
+/*===============================================================================*/
 // Conexão com rede WifFi
 void WiFiConnect(){
     // Desconexão de uma rede anterior
     WiFi.disconnect();
 
     // Configura ESP32 como extação de WiFi sem desabilitar demais modos
-    // WiFi.config(LocalIP, Gateway, Subnet);
     WiFi.enableSTA(true);
     delay(2000);
 
@@ -55,15 +55,29 @@ void handleRoot(){
 }
 
 // Função que atualiza dados enviados do servidor para o cliente
-void handleUpdate(){
+void handleUpdateReceived(){
   Server.send(200, "text/plain"/*texto normal*/, Sended);
+}
+
+// Função que atualiza dados enviados do servidor para o cliente
+void handleUpdateTemp(){
+  Server.send(200, "text/plain"/*texto normal*/, Temperature);
+}
+
+// Função que atualiza dados enviados do servidor para o cliente
+void handleUpdateHum(){
+  Server.send(200, "text/plain"/*texto normal*/, Humidity);
 }
 
 // Criação do Server
 void createServer(){
-  Server.on("/", handleRoot);
-  Server.on("/update", handleUpdate);
-  Server.begin();
+  Server.on("/", handleRoot);               // lida com atualização de página
+  Server.on("/received", HTTP_GET, handleUpdateReceived);     // lida com atualização de dado recebido pelo cliente
+  Server.on("/temp", HTTP_GET, handleUpdateTemp);     // lida com atualização da temperatura ambiente
+  Server.on("/hum", HTTP_GET ,handleUpdateHum);       // lida com atualização da humdiade do ambiente
+  Server.begin();                           // inicia server
+
+  // Avisa que server começou
   Serial.println("HTTP Server started!");
   Serial.println("\nChat:");
 }
@@ -85,10 +99,38 @@ void sendData(){
   Serial.println("Send: " + Sended);
 }
 
+/*===============================================================================*/
+void setupDHT(){
+  DHT.temperature().getSensor(&Sensor);
+  DHT.humidity().getSensor(&Sensor);
+  delayDHT = Sensor.min_delay / 1000;
+}
+
+void updateValueDHT(){
+  
+  sensors_event_t event;
+  float temperature, humidity;
+
+  DHT.temperature().getEvent(&event);
+  if(isnan(event.temperature)) temperature = -1;
+  else temperature = event.temperature;
+
+  DHT.temperature().getEvent(&event);
+  if(isnan(event.relative_humidity)) humidity = -1;
+  else humidity = event.temperature;
+
+  Temperature = String(temperature); Humidity = String(humidity);
+
+}
+
+/*===============================================================================*/
 void setup() {
 
-  // Condigura pino do led como saída do sistema
+  // Configura pino do led como saída do sistema
   pinMode(PIN_LED, OUTPUT);
+
+  // Configura sensor DHT
+  setupDHT();
 
   // Configuração do monitor serial
   Serial.begin(115200);
@@ -102,13 +144,18 @@ void setup() {
 
 void loop() {
 
-  // verifica se há requisições de novos clientes e configura rotas adequadas
+  // Verifica se há requisições de novos clientes e configura rotas adequadas
   handleClient();
+
+  // Atualização com dados do sensoriamento de temperatura e humidade
+  // updateValueDHT();
 
   // Se ouver dados do serial, são lidos e transmitidos para página HTML
   if(Serial.available()){
     sendData();  
   }
 
-  delay(100);
+  // Serial.println("Humidity: " + Temperature + "ºC  Temperature: " + Humidity);
+  delay(delayMS);
+
 }
